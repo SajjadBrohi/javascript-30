64a0a190459ec02f65d1b6dfc61aa1ea8c993331
unknown <s.brohi@uq.net.au>
2020-02-11 06:04:30 +1000

    New Projects

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -3,3 +3,30 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+class Code {
+    constructor(revenent) {
+        this.revenent = revenent;
+        this.simka = 0;
+        this.sick = function () {
+            return this.revenent * 3
+        }
+    }
+
+    speak() {
+        console.log("I'm here")
+    }
+}
+
+class SubCode extends Code {
+    constructor(revenent) {
+        super(revenent)
+    }
+
+    speak() {
+        console.log("I'm Now here")
+    }
+}
+
+
+let coder = new Code(3);
