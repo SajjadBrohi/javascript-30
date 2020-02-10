69f0987c8e11993574ea70fdab01a809b765213c
unknown <s.brohi@uq.net.au>
2020-02-10 14:04:05 +1000

    Completed project

--- Mouse Move Shadow/script.js
+++ Mouse Move Shadow/script.js
@@ -1,11 +1,20 @@
 const HERO = document.querySelector(".hero");
 const CONTENT = HERO.querySelector("h1");
 
-
-
 function mouseMoveOnHero(e) {
-    console.log(e);
-    
+
+    let xCentre = window.innerWidth / 2;
+    let yCentre = window.innerHeight / 2;
+
+    let xAxis = (e.x - xCentre) / 2;
+    let yAxis = (e.y - yCentre) / 2;
+
+    CONTENT.style.textShadow = `
+        ${xAxis}px ${yAxis}px 0 rgb(255, 121, 233), 
+        ${xAxis * -1}px ${yAxis}px 0 blue, 
+        ${xAxis}px ${yAxis * -1}px 0 rgb(107, 218, 255), 
+        ${xAxis * -1}px ${yAxis * -1}px 0 rgb(87, 255, 87)
+    `
 }
 
 
