2da866e4d174c997b070f60cc5d391d8c577b63b
unknown <s.brohi@uq.net.au>
2020-02-11 00:58:29 +1000

    New Projects

--- Sort Without Articles/script.js
+++ Sort Without Articles/script.js
@@ -1,24 +1,14 @@
 const BANDS_TAG = document.querySelector("#bands");
 
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything',
     'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-
-
 function slice(string) {
-    if (string.startsWith("The ")) {
-        return string.slice(4);
-    } else if (string.startsWith("An ")) {
-        return string.slice(3);
-    } else if (string.startsWith("A ")) {
-        return string.slice(2);
-    } else {
-        return string
-    }
+    return string.replace(/^(an |a |the )/i, '').trim();
 }
 
-bands.sort((a, b) => slice(a) > slice(b) ? 1 : -1 );
+bands.sort((a, b) => slice(a) > slice(b) ? 1 : -1);
 
-BANDS_TAG.innerHTML = 
- bands.map((band) =>  `<li> ${band} </li>`).join('');
+BANDS_TAG.innerHTML =
+    bands.map((band) => `<li> ${band} </li>`).join('');
 
