1cbab6b0ce82c1c4c2fa6b365c0fd9c1b8066ad3
unknown <s.brohi@uq.net.au>
2020-02-16 02:07:01 +1000

    Added event capture project

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,41 +1,44 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function formSubmit(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    let itemName = this.querySelector("input[type='text']").value;
+	let itemName = this.querySelector("input[type='text']").value;
 
-    let item = {
-        itemName,
-        checked: false
-    }
+	let item = {
+		itemName,
+		checked: false
+	};
 
-    items.push(item);
-    itemsAdd(itemsList, items);
-    localStorage.setItem('items', JSON.stringify(items));
-    this.reset();
+	items.push(item);
+	itemsAdd(itemsList, items);
+	localStorage.setItem("items", JSON.stringify(items));
+	this.reset();
 }
 
 function itemsAdd(plates, plateList) {
-    plates.innerHTML = plateList.map((plate, i) => {
-        return `
+	plates.innerHTML = plateList
+		.map((plate, i) => {
+			return `
         <li>
-            <input type="checkbox" data-index=${i} id= "item${i}" ${plate.checked ? 'checked' : ''} />
+            <input type="checkbox" data-index=${i} id= "item${i}" ${
+				plate.checked ? "checked" : ""
+			} />
             <label for="item${i}">${plate.itemName}</label>
-        </li>`
-    }).join("");
+        </li>`;
+		})
+		.join("");
 }
 
 function checkboxStorage(e) {
-    if (!e.target.matches("input")) return;
-    const el = e.target;
-    const index = el.dataset.index;
-    items[index].checked = !items[index].checked;
-    localStorage.setItem('items', JSON.stringify(items));
-    itemsAdd(itemsList, items)
-
+	if (!e.target.matches("input")) return;
+	const el = e.target;
+	const index = el.dataset.index;
+	items[index].checked = !items[index].checked;
+	localStorage.setItem("items", JSON.stringify(items));
+	itemsAdd(itemsList, items);
 }
 
 addItems.addEventListener("submit", formSubmit);
