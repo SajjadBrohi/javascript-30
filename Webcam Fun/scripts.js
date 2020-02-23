6df3467dbfe8cf82c1b7ebce52460ed58a45d9af
unknown <s.brohi@uq.net.au>
2020-02-23 23:57:44 +1000

    Made changes to project

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -1,32 +1,31 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
 
 class Code {
-    constructor(revenent) {
-        this.revenent = revenent;
-        this.simka = 0;
-        this.sick = function () {
-            return this.revenent * 3
-        }
-    }
+	constructor(revenent) {
+		this.revenent = revenent;
+		this.simka = 0;
+		this.sick = function() {
+			return this.revenent * 3;
+		};
+	}
 
-    speak() {
-        console.log("I'm here")
-    }
+	speak() {
+		console.log("I'm here");
+	}
 }
 
 class SubCode extends Code {
-    constructor(revenent) {
-        super(revenent)
-    }
+	constructor(revenent) {
+		super(revenent);
+	}
 
-    speak() {
-        console.log("I'm Now here")
-    }
+	speak() {
+		console.log("I'm Now here");
+	}
 }
-
 
 let coder = new Code(3);
