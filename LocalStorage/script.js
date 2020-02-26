0f72dd54e9b9eb06f94e1e0d5fce69e9b0306635
unknown <s.brohi@uq.net.au>
2020-02-26 15:00:57 +1000

    Used prettier

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,6 +1,6 @@
-const addItems = document.querySelector(".add-items");
-const itemsList = document.querySelector(".plates");
-const items = JSON.parse(localStorage.getItem("items")) || [];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function formSubmit(e) {
 	e.preventDefault();
@@ -9,12 +9,12 @@ function formSubmit(e) {
 
 	let item = {
 		itemName,
-		checked: false
+		checked: false,
 	};
 
 	items.push(item);
 	itemsAdd(itemsList, items);
-	localStorage.setItem("items", JSON.stringify(items));
+	localStorage.setItem('items', JSON.stringify(items));
 	this.reset();
 }
 
@@ -24,24 +24,24 @@ function itemsAdd(plates, plateList) {
 			return `
         <li>
             <input type="checkbox" data-index=${i} id= "item${i}" ${
-				plate.checked ? "checked" : ""
+				plate.checked ? 'checked' : ''
 			} />
             <label for="item${i}">${plate.itemName}</label>
         </li>`;
 		})
-		.join("");
+		.join('');
 }
 
 function checkboxStorage(e) {
-	if (!e.target.matches("input")) return;
+	if (!e.target.matches('input')) return;
 	const el = e.target;
 	const index = el.dataset.index;
 	items[index].checked = !items[index].checked;
-	localStorage.setItem("items", JSON.stringify(items));
+	localStorage.setItem('items', JSON.stringify(items));
 	itemsAdd(itemsList, items);
 }
 
-addItems.addEventListener("submit", formSubmit);
-itemsList.addEventListener("click", checkboxStorage);
+addItems.addEventListener('submit', formSubmit);
+itemsList.addEventListener('click', checkboxStorage);
 
 itemsAdd(itemsList, items);
