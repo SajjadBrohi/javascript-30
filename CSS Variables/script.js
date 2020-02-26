db8416bbc9ec56a2716ea4ed8f338e8efd0da155
unknown <s.brohi@uq.net.au>
2020-02-26 14:59:43 +1000

    Used Prettier

--- CSS Variables/script.js
+++ CSS Variables/script.js
@@ -1,7 +1,7 @@
-const SPACING = document.querySelector("#spacing");
-const BLUR = document.querySelector("#blur");
-const COLOR = document.querySelector("#base");
-const CONTROLS = document.querySelector(".controls");
+const SPACING = document.querySelector('#spacing');
+const BLUR = document.querySelector('#blur');
+const COLOR = document.querySelector('#base');
+const CONTROLS = document.querySelector('.controls');
 const ROOT = document.documentElement;
 
 function changeValues() {
@@ -9,10 +9,10 @@ function changeValues() {
 	let blurValue = BLUR.value;
 	let colorValue = COLOR.value;
 
-	ROOT.style.setProperty("--space", `${spacingValue}px`);
-	ROOT.style.setProperty("--blur", `${blurValue}px`);
-	ROOT.style.setProperty("--base", `${colorValue}`);
+	ROOT.style.setProperty('--space', `${spacingValue}px`);
+	ROOT.style.setProperty('--blur', `${blurValue}px`);
+	ROOT.style.setProperty('--base', `${colorValue}`);
 	console.log(spacingValue);
 }
 
-CONTROLS.addEventListener("input", changeValues, false);
+CONTROLS.addEventListener('input', changeValues, false);
